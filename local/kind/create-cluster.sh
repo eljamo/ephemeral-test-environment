91805ab6f29119dc06f8e29d1781be77279316d6
Ellis James <contact@eljamo.com>
2025-08-12 15:53:55 +0100

    ci: tweaks

--- local/kind/create-cluster.sh
+++ local/kind/create-cluster.sh
@@ -3,25 +3,30 @@
 echo "Creating new kind cluster"
 echo ""
 
-kind create cluster --config ./config.yaml --name dev
+kind create cluster --name dev --config ./config.yaml
 
 echo ""
-echo "Appling Ingress NGINX"
+echo "Installing Ingress NGINX"
 echo ""
 
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 
 echo ""
-echo "Installng argo-workflows in namespace argo"
+
+until kubectl get pods -n ingress-nginx -l app.kubernetes.io/component=controller 2>/dev/null | grep -q .; do
+  sleep 2
+done
+
+kubectl wait --namespace ingress-nginx \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=90s
+
+echo ""
+echo "Installng argo-workflows"
 echo ""
 
 helm install argo-workflows argo/argo-workflows -n argo --create-namespace --wait
-
-# echo ""
-# echo "Applying Argo Workflows Ingress"
-# echo ""
-
-# kubectl apply -f ./argo-workflow-ingress.yaml
 
 echo ""
 echo "Creating roles for service accounts"
